fc6f1347d09cdb2c17e3d4248c3857e7eadf3357
Burak YAPICI <burakyapici747@gmail.com>
2022-11-11 17:37:48 +0300

    Relations

--- src/main/java/com/wordrace/model/Game.java
+++ src/main/java/com/wordrace/model/Game.java
@@ -2,9 +2,8 @@ package com.wordrace.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Entity
@@ -13,6 +12,17 @@ public class Game {
 
     @Id
     private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "roomId", referencedColumnName = "id")
+    private Room room;
+
+    @OneToMany(mappedBy = "game")
+    private List<UserScore> userScore;
+
+    @ManyToMany(targetEntity = Word.class)
+    private List<Word> words;
+
     private int totalScore;
 
 }
