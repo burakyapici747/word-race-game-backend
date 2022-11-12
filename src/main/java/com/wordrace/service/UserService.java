aa2d450c2bebf3e4094e2f2b726eab0d5075d359
yapicimurat <muratyapiicii@gmail.com>
2022-11-12 16:24:58 +0300

    Create User service

---
+++ src/main/java/com/wordrace/service/UserService.java
@@ -0,0 +1,38 @@
+package com.wordrace.service;
+
+import com.wordrace.model.Game;
+import com.wordrace.model.Room;
+import com.wordrace.model.User;
+import java.util.List;
+import java.util.Optional;
+
+public interface UserService {
+
+
+    //GET OPERATIONS
+    Optional<List<User>> getAllUsers();
+
+    Optional<User> getUserById(Long id);
+
+    Optional<Game> getAllGameByUserId(Long userId);
+
+    Optional<List<Room>> getAllRoomsByUserId(Long userId);
+
+
+    //POST OPERATIONS
+
+    Optional<User> createUser(User user);
+
+    Optional<Room> joinRoom(Long userId, Long roomId);
+
+    Optional<Room> addScoreToUser(Long userId, Long gameId, int score);
+
+    //PUT OPERATIONS
+
+    Optional<User> updateUser(User user);
+
+    //DELETE OPERATIONS
+
+    boolean deleteUserById(Long userId);
+
+}
