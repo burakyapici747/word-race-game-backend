dddf6a3cc76757090aa111cf78406b0b60e1cb96
Burak YAPICI <burakyapici747@gmail.com>
2022-11-12 16:44:15 +0300

    Created Room Service

---
+++ src/main/java/com/wordrace/service/RoomService.java
@@ -0,0 +1,28 @@
+package com.wordrace.service;
+
+import com.wordrace.model.Game;
+import com.wordrace.model.Room;
+import com.wordrace.model.Word;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RoomService {
+
+    //Get Operations
+    Optional<List<Room>> getAllRooms();
+    Optional<Room> getRoomById(Long id);
+    Optional<Game> getGameByRoomId(Long roomId);
+    Optional<List<Word>> getWordsByRoomId(Long roomId);
+
+    //Post Operations
+
+    Optional<Room> createRoom();
+
+    //Put Operations
+    Optional<Room> updateRoomByRoomId(Long roomId, Room room);
+
+    //Delete Operations
+    boolean deleteRoomById(Long id);
+
+}
