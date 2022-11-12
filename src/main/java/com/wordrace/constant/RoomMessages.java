49c3ed8471ddee48a92ea4983996913c058a6d26
yapicimurat <muratyapiicii@gmail.com>
2022-11-12 22:33:22 +0300

    Result messages update and create new result message types

---
+++ src/main/java/com/wordrace/constant/RoomMessages.java
@@ -0,0 +1,7 @@
+package com.wordrace.constant;
+
+public final class RoomMessages {
+
+    public static final String ROOM_CAPACITY_IS_FULL = "Room capacity is full.";
+
+}
---
+++ src/main/java/com/wordrace/constant/WordMessages.java
@@ -0,0 +1,4 @@
+package com.wordrace.constant;
+
+public class WordMessages {
+}
