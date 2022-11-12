9dc82e626ff9af914ee2f4cd2bc13cd60bc88e13
yapicimurat <muratyapiicii@gmail.com>
2022-11-12 22:42:43 +0300

    Add find user by nickname, by email

--- src/main/java/com/wordrace/repository/UserRepository.java
+++ src/main/java/com/wordrace/repository/UserRepository.java
@@ -3,9 +3,13 @@ package com.wordrace.repository;
 import com.wordrace.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findUserByNickname(String nickName);
 
+    Optional<User> findUserByEmail(String email);
 
 
 }
