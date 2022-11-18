973bfe96506713654e3d928bdfba0959c08fa788
yapicimurat <muratyapiicii@gmail.com>
2022-11-18 23:05:53 +0300

    Refactoring Dto classes

--- src/main/java/com/wordrace/dto/UserScoreDto.java
+++ src/main/java/com/wordrace/dto/UserScoreDto.java
@@ -1,8 +1,10 @@
 package com.wordrace.dto;
 
+import lombok.Data;
+
+@Data
 public class UserScoreDto {
     private Long id;
-    private GameDto game;
     private UserDto user;
     private int score;
 }
