e21772876afba83119f6151ee3030d77f26d2fc6
yapicimurat <muratyapiicii@gmail.com>
2022-11-18 23:13:39 +0300

    Adding some new response messages for Room

--- src/main/java/com/wordrace/constant/RoomMessages.java
+++ src/main/java/com/wordrace/constant/RoomMessages.java
@@ -4,5 +4,7 @@ public final class RoomMessages {
 
     public static final String ROOM_CAPACITY_IS_FULL = "Room capacity is full.";
     public static final String ROOM_NAME_ALREADY_EXIST = "Room name is already exits.";
-
+    public static final String ROOM_USER_ALREADY_IN = "User is already in.";
+    public static final String ROOM_USER_NOT_JOINED = "This user is not joined room.";
+    public static final String ROOM_HAS_ALREADY_GAME = "Room has already a game.";
 }
