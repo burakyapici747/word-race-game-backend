ba3312db4df247ccbb70ec0886a5def266941655
Ümit Sayın <umit.sayin@freightest.com>
2022-11-23 10:19:18 +0300

    Refactoring Id type to UUID and cleaning space area

--- src/main/java/com/wordrace/model/BaseEntity.java
+++ src/main/java/com/wordrace/model/BaseEntity.java
@@ -2,6 +2,7 @@ package com.wordrace.model;
 
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
@@ -15,8 +16,10 @@ public abstract class BaseEntity implements Serializable {
     private static final long serialVersionUID = 4845892487153272010L;
 
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(generator = "uuid4")
+    @GenericGenerator(name = "uuid4", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id",columnDefinition = "char(64)")
+    @Type(type="org.hibernate.type.UUIDCharType")
     private UUID id;
 
     @CreatedDate
